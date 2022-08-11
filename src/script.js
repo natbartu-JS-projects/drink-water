d74dba87414c1b19b95958678f5331a04ca2a943
natacha <nat_bartu@hotmail.com>
2022-08-11 12:26:23 +1000

    add a event listener for loop each cup with correspondent index

--- src/script.js
+++ src/script.js
@@ -2,3 +2,7 @@ const smallCups = document.querySelectorAll(".cup-small");
 const liters = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => highlightCups(idx));
+});
